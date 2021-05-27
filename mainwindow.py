3f5fc91422ca6a0e62f52c6bb29313f063cc0728
dev2810 <devaditya.tripathi@gmail.com>
2021-05-27 20:16:40 +0530

    Main window for professional account

--- mainwindow.py
+++ mainwindow.py
@@ -1,9 +1,21 @@
 from tkinter import *
+from tkinter import Frame
+from PIL import Image, ImageTk
 main_window=Tk()
 main_window.geometry('1920x1080')
 main_window.state('zoomed')
-frame1=Frame(main_window,bg='PaleGreen1')
-frame1.pack(side=TOP,fill=X)
-Label(frame1,text='fenk').pack()
+
+#TITLEBAR
+#Frame
+frame1=Frame(main_window,bg='PaleGreen1', highlightbackground="dark green",highlightthickness=1)
+frame1.grid(row=0,column=0,columnspan=1000,rowspan=1,sticky=NSEW)
+Label(frame1,text='Button',bg='PaleGreen1',font=('TkDefaultFont',16),padx=10,pady=5,justify='right').grid(row=0,column=1,sticky=E)
+Label(frame1,text='*Name*',bg='PaleGreen1',font=('TkDefaultFont',16),padx=10,pady=5).grid(row=0,column=0,sticky=W)
+
+#Image
+bgimg=ImageTk.PhotoImage(Image.open('bgimage.jpg'))
+Label(main_window,image=bgimg ,borderwidth=1).grid(row=1,column=0)
+
+
 
 main_window.mainloop()
